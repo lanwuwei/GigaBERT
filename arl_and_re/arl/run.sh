14393b4977e9cf5af3b32d780f4176b64f38b38c
Wuwei Lan <lwwscc@gmail.com>
2020-10-08 19:20:29 -0400

    update code repo

--- arl_and_re/arl/run.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-CUDA_VISIBLE_DEVICES=0 python main.py --exp_name bibert-124k-en-ar --bert_model bibert-124k --gpuid 0 --batchsize 8 --warmup_proportion 0.20000 --learning_rate 0.00002 --max_epoch 7 --target_language ar --source_language en --seed 0 --num_duplicate 20 --max_seq_length 128
--- arl_and_re/re/run.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-CUDA_VISIBLE_DEVICES=1 python main.py --exp_name bibert-124k-en-ar --bert_model bibert-124k  --gpuid 0 --batchsize 4 --warmup_proportion 0.20000 --learning_rate 0.00002 --max_epoch 7 --target_language ar --source_language en --seed 1234 --num_duplicate 20 --max_seq_length 512
